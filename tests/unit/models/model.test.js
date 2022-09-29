ef78e0cc3107d5fb4b42aac693e0afe6484ca58c
cafe51 <cafecafe51@hotmail.com>
2022-09-29 14:13:32 -0400

    teste camada model criado e mock criado

---
+++ tests/unit/models/model.test.js
@@ -0,0 +1,30 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const connection = require('../../../src/models/connection');
+const { productModel } = require('../../../src/models/index');
+
+const productsMock = require('../models/mocks/products.model.mocks');
+
+// queryAllProducts
+// queryProductById
+
+describe('testando o SELECT do model', function () {
+  afterEach(sinon.restore);
+  // console.log(productsMock);
+  it('retorna todos produtos', async function () {
+    sinon.stub(connection, 'execute').resolves([productsMock]);
+    const result = await productModel.queryAllProducts();
+    // console.log('RESULTADO DO TESTE', result);
+    expect(result).to.equal(productsMock);
+  });
+
+  it('retorna produto por id', async function () {
+    sinon.stub(connection, 'execute').resolves([productsMock[0]]);
+    const result = await productModel.queryProductById(1);
+    console.log('resultado queryProductById(1)', result);
+    expect(result).to.be.deep.equal(productsMock[0]);
+
+  });
+
+});
