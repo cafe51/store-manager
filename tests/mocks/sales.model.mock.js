25958996ba46397956efdc5c418f3aa7bda41643
cafe51 <cafecafe51@hotmail.com>
2022-10-05 02:30:44 -0400

    mock criado

--- tests/mocks/sales.model.mock.js
+++ tests/mocks/sales.model.mock.js
@@ -79,6 +79,19 @@ const salesListMock = [
   }
 ]
 
+const responseOfGetSalesByIdMock = [
+  {
+    "date": "2022-10-05T06:24:41.000Z",
+    "productId": 1,
+    "quantity": 5
+  },
+  {
+    "date": "2022-10-05T06:24:41.000Z",
+    "productId": 2,
+    "quantity": 10
+  }
+]
+
 
 module.exports = {
   newSaleMock,
